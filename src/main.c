3ebd388108727826bce685122a8dd283f2ed6a10
Julian Sparber <julian@sparber.net>
2017-10-01 21:57:28 +0200

    [fix] flatpak hack to get XDG DIR DOWNLOAD to work

--- src/main.c
+++ src/main.c
@@ -5,5 +5,9 @@
 int
 main (int argc, char *argv[])
 {
+  if (g_get_user_special_dir(G_USER_DIRECTORY_DOWNLOAD) == NULL) {
+    g_spawn_command_line_sync("xdg-user-dirs-update", NULL, NULL, NULL, NULL);
+  }
+
   return g_application_run (G_APPLICATION (teleport_app_new ()), argc, argv);
 }
