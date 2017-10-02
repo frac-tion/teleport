d214334feeade5f3ebc84febf5234ceffd8d4920
Julian Sparber <julian@sparber.net>
2017-10-02 20:34:06 +0200

    [refactor] code intent

--- src/teleport-window.h
+++ src/teleport-window.h
@@ -5,15 +5,19 @@
 #include "teleport-app.h"
 #include "teleport-peer.h"
 
-
 #define TELEPORT_APP_WINDOW_TYPE (teleport_app_window_get_type ())
-G_DECLARE_FINAL_TYPE (TeleportAppWindow, teleport_app_window, TELEPORT, APP_WINDOW, GtkApplicationWindow)
+G_DECLARE_FINAL_TYPE (TeleportAppWindow, 
+                      teleport_app_window, 
+                      TELEPORT, 
+                      APP_WINDOW, 
+                      GtkApplicationWindow)
 
-  TeleportAppWindow       *teleport_app_window_new          (TeleportApp *app);
-  void                    teleport_app_window_open         (TeleportAppWindow *win,
-      GFile            *file);
-  extern void update_remote_device_list(TeleportAppWindow *, Peer *);
-  extern void update_remote_device_list_remove(TeleportAppWindow *, Peer *);
-
+TeleportAppWindow *teleport_app_window_new          (TeleportApp       *app);
+void               teleport_app_window_open         (TeleportAppWindow *win,
+                                                     GFile             *file);
+void               update_remote_device_list        (TeleportAppWindow *,
+                                                     Peer              *);
+void               update_remote_device_list_remove (TeleportAppWindow *,
+                                                     Peer              *);
 
 #endif /* __TELEPORT_WINDOW_H */
