ad62b3b3366bd3d4a58795c7d09bc12d9c80dee5
Julian Sparber <julian@sparber.net>
2017-10-02 20:37:43 +0200

    [refactor] code intent

--- src/teleport-app.h
+++ src/teleport-app.h
@@ -9,12 +9,12 @@ G_DECLARE_FINAL_TYPE (TeleportApp, teleport_app, TELEPORT, APP, GtkApplication)
 
 
 TeleportApp     *teleport_app_new            (void);
-extern void     create_user_notification     (const char *,
+void            create_user_notification     (const char *,
                                               const int,
                                               const char *,
                                               GVariant *);
 
-extern void     create_finished_notification (const char *,
+void            create_finished_notification (const char *,
                                               const int,
                                               const char *,
                                               GVariant *);
